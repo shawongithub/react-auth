923d50469693edc65e1e2d09a7986ae662f0d065
shawon <shawonislam121009@gmail.com>
2021-07-07 14:32:13 +0600

    test commit

---
+++ src/components/Templates/Templates.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import background from '../../images/Bg.png'
+const Templates = () => {
+
+    return (
+        <div style={{
+            backgroundImage: `url(${background})`
+        }} >
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
+            </p>
+        </div>
+    );
+};
+
+export default Templates;
