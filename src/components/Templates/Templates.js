843a42de561e89401c9806926b5c313b7790d828
shawon <shawonislam121009@gmail.com>
2021-07-07 14:54:05 +0600

    added background image

--- src/components/Templates/Templates.js
+++ src/components/Templates/Templates.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import background from '../../images/Bg.png'
 const Templates = () => {
-
+    const backgroundStyle = {
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        height: '768px',
+    }
     return (
-        <div style={{
-            backgroundImage: `url(${background})`
-        }} >
+        <div style={backgroundStyle} >
             <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore quibusdam dolores, mollitia saepe qui, nisi unde provident, voluptas perferendis nulla. Eligendi culpa, nihil veritatis ipsum eum recusandae explicabo provident!
             </p>
