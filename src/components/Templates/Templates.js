49869a13a10560d143964d2ba1d211e6bf2956a3
shawon <shawonislam121009@gmail.com>
2021-07-10 16:43:15 +0600

    added fakeData

--- src/components/Templates/Templates.js
+++ src/components/Templates/Templates.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import background from '../../images/Bg.png'
+import { traffics } from '../../fakeData/traffics';
 const Templates = () => {
     const backgroundStyle = {
         backgroundImage: `url(${background})`,
         backgroundSize: 'cover',
         height: '800px'
     }
+    console.log(traffics);
     return (
         <div style={backgroundStyle} >
-            <div style={{ marginTop: '0px' }}>
-                <span style={{ color: '#88def4' }}>no top margin</span>
+            <div>
+                <span style={{ color: '#88def4' }}>no empty space for background</span>
+                {
+                    traffics.map(traffic => {
+                        return (<div>
+                            <p>{traffic.id}</p>
+                            <h1>{traffic.name}</h1>
+                            <img src={traffic.image} alt="" />
+                        </div>)
+                    })
+                }
 
             </div>
         </div>
---
+++ src/fakeData/traffics.js
@@ -0,0 +1,27 @@
+import bike from '../images/Frame.png'
+import car from '../images/Frame-2.png'
+import bus from '../images/Frame-1.png'
+import train from '../images/Group.png'
+
+export const traffics = [
+    {
+        id: 1,
+        name: "BIKE",
+        image: bike
+    },
+    {
+        id: 2,
+        name: "CAR",
+        image: car
+    },
+    {
+        id: 3,
+        name: "BUS",
+        image: bus
+    },
+    {
+        id: 4,
+        name: "TRAIN",
+        image: train
+    }
+]
