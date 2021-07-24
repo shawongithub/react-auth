6b1d31d85fe7dfbe16f4efd5faaa0329625ebc5d
shawon <shawonislam121009@gmail.com>
2021-07-24 09:07:18 +0600

    perfected the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className="header-container">
-            <h2>City Travellers</h2>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/destination'>Destination</Link>
-                <Link to='/blog'>Blog</Link>
-                <Link to='/contact'>Contact</Link>
-                <Link to='/login'>Login</Link>
-            </nav>
+        <div className="header-background">
+            <div className="header-container">
+                <h2>City Travellers</h2>
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/destination'>Destination</Link>
+                    <Link to='/blog'>Blog</Link>
+                    <Link to='/contact'>Contact</Link>
+                    <Link to='/login'>Login</Link>
+                </nav>
+            </div>
+
         </div>
     );
 };
