38029c060c126e26dd8f9e65cab0412aba543ed7
shawon <shawonislam121009@gmail.com>
2021-07-28 10:01:06 +0600

    firebase config and email password registration

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,42 +2,76 @@ import React from 'react';
 import './SignUp.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../firebaseConfig'
+
+firebase.initializeApp(firebaseConfig);
 
 const SignUp = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [userInfo, setUserInfo] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
 
-    const changeHandler = e => {
-        if (e.target.name === "name") {
-            setName(e.target.value)
-        }
+    const blurHandler = e => {
+
+        let isFieldValid = true
         if (e.target.name === "email") {
-            console.log(e.target.value);
-            setEmail(e.target.value)
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value)
+        }
+        if (e.target.name === "password") {
+            isFieldValid = e.target.value.length > 5 && /\d{1}/.test(e.target.value)
+        }
+        if (e.target.name === "confirmPassword") {
+            const isFieldValid = e.target.value === userInfo.password
+            if (!isFieldValid) {
+                alert("please enter the same  password")
+                e.target.value = null
+            }
+        }
+        if (isFieldValid) {
+            const newUserInfo = { ...userInfo }
+            newUserInfo[e.target.name] = e.target.value
+            setUserInfo(newUserInfo)
         }
     }
-    const emailValidator = email => {
-        console.log(email);
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
+
     const signUpHandler = e => {
         e.preventDefault()
-        let test = emailValidator(email)
-        console.log(test);
-        console.log("clicked");
+        console.log(userInfo);
+        if (userInfo.email && userInfo.password) {
+            console.log("welcome to be registered");
+            firebase.auth().createUserWithEmailAndPassword(userInfo.email, userInfo.password)
+                .then((userCredential) => {
+
+                    var user = userCredential.user;
+                    console.log(user);
+
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode);
+                    console.log(errorMessage);
+
+                });
+        }
     }
-    console.log(email);
+
     return (
         <div>
+
             <div className="form-container">
                 <form onSubmit={signUpHandler}>
                     <h1>SignUp Here</h1>
-                    <input type="text" placeholder="Name" name="name" onChange={changeHandler} required />
-                    <input type="text" placeholder="Email" name="email" onChange={changeHandler} required />
-                    <input type="password" placeholder="Password" name="password" required />
-                    <input type="password" placeholder="Confirm Password" name="confirmPassword" required />
+                    <input type="text" placeholder="Name" name="name" onBlur={blurHandler} required />
+                    <input type="text" placeholder="Email" name="email" onBlur={blurHandler} required />
+                    <input type="password" placeholder="Password" name="password" onBlur={blurHandler} required />
+                    <input type="password" placeholder="Confirm Password" name="confirmPassword" onBlur={blurHandler} required />
                     <input type="submit" value="Create an account" />
                     <p>Already have an account? <Link to="/login">Login</Link></p>
                 </form>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvCh68cuCPZGok4DAeGZlGYW3299-Q4uw",
+    authDomain: "auth-assignment-7e1fd.firebaseapp.com",
+    projectId: "auth-assignment-7e1fd",
+    storageBucket: "auth-assignment-7e1fd.appspot.com",
+    messagingSenderId: "307383923212",
+    appId: "1:307383923212:web:bf20ed5e54432b1f9b3429",
+    measurementId: "G-RJEQL3GXZ6"
+};
+
+export default firebaseConfig
