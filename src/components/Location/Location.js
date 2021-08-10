025eb8470f212bc73b651b5d53e758e4a7148fe0
shawon <shawonislam121009@gmail.com>
2021-08-10 08:36:49 +0600

    styled the search location page

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import mapImage from '../../images/Map.png'
+import './Location.css'
 const Location = () => {
-    const style = {
-        margin: '5px 100px',
-        border: '1px solid red',
-        display: 'flex'
-    }
-    const searchLoactionForm = {
-        width: '350px',
-        height: '220px',
-        border: '1px solid lightGrey',
-        marginRight: '20px'
-    }
+
     return (
-        <div style={style}>
-            <div style={searchLoactionForm}>
+        <div className="search-location-container">
+            <div className="search-location-form">
                 <form >
                     <label htmlFor="pickup">Pick From</label>
+                    <br />
                     <input type="text" name="pickup" required />
+                    <br />
                     <label htmlFor="destination">Pick To</label>
+                    <br />
                     <input type="text" name="destination" required />
                     <input type="submit" value="Search" />
                 </form>
